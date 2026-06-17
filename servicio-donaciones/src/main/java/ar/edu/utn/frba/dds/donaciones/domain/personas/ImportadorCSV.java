b86755ba848b1182313567de6bba3ba2684abbc0
Brandon <bquenallata@frba.utn.edu.ar>
2026-06-17 15:29:16 -0300

    primeros cambios en implementación de importador

--- servicio-donaciones/src/main/java/ar/edu/utn/frba/dds/donaciones/domain/personas/ImportadorCSV.java
+++ servicio-donaciones/src/main/java/ar/edu/utn/frba/dds/donaciones/domain/personas/ImportadorCSV.java
@@ -1,9 +1,29 @@
 package ar.edu.utn.frba.dds.donaciones.domain.personas;
-
-import java.io.File;
-
+import com.opencsv.*;
+import java.io.*;
+import java.util.List;
+import java.util.ArrayList;
 public class ImportadorCSV {
-    public void importar(File archivo){
+    FileReader archCSV=null;
+    CSVReader csvReader=null;
+
+    public void importar(String rutaArchivo){
+        try{
+        archCSV = new FileReader(rutaArchivo);
+        csvReader = new CSVReader(archCSV);
+        String[] fila=null;
+        while ((fila=csvReader.readNext())!=null){
+                System.out.println(fila[0]
+              + " | " + fila[1]
+              + " |  " + fila[2]);
+        }
+        }
+        finally{
+            csvReader.Close();
+        }
+    }
+
+    public registrarDonante(String email){
 
     }
 }
