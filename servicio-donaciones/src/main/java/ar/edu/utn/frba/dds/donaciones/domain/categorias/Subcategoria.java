aecaf18ac94a93cbf279321d1af968e874429620
agustfuentes <87042865+agfuentes@frba.utn.edu.ar>
2026-06-19 08:07:07 -0300

    Ajusta subcategoria para atributos y nombre

--- servicio-donaciones/src/main/java/ar/edu/utn/frba/dds/donaciones/domain/categorias/Subcategoria.java
+++ servicio-donaciones/src/main/java/ar/edu/utn/frba/dds/donaciones/domain/categorias/Subcategoria.java
@@ -3,15 +3,23 @@ package ar.edu.utn.frba.dds.donaciones.domain.categorias;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 @Setter
 public class Subcategoria {
     private String nombre;
+    private List<DefinicionAtributo> atributosAsociados = new ArrayList<>();
 
     public Subcategoria() {
     }
 
     public Subcategoria(String nombre) {
         this.nombre = nombre;
+    }
+
+    public void agregarAtributo(DefinicionAtributo atributo) {
+        atributosAsociados.add(atributo);
     }
 }
